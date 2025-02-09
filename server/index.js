0d05499520e79c56b3b012165c703339be488c28
ashlei-t <a.temena@gmail.com>
2025-02-09 22:08:37 +0100

    server setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+const port = 5001
+
+dotenv.config()
+
+const spotify_client_id = process.env.VITE_SPOTIFY_CLIENT_ID
+const spotify_client_secret = process.env.VITE_SPOTIFY_CLIENT_SECRET
+
+const app = express();
+
+app.get('/auth/login', (req, res) => {
+});
+
+app.get('/auth/callback', (req, res) => {
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import 'vite/modulepreload-polyfill'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
